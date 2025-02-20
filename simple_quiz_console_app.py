0a2b202a4e4317f4230b22b09531a112ca1d831d
Nikita <hoper347were@gmail.com>
2025-02-20 22:27:45 +0300

    OUTPUT correction, cosmetic adjustments

--- simple_quiz_console_app.py
+++ simple_quiz_console_app.py
@@ -44,22 +44,20 @@ num_2 = random.randrange(1, 100)
 operator = random.choice(math_operators)
 
 # print task for user to solve
-
+print("=" * 100)
 print(num_1, operator, num_2)
 
 # input answer
-
 given_answer = check_input()
-
+first_input = True
 # I won't explain it...
 # Ok... Nah
 
 while given_answer != "q":
     count = 0                                                                        # to count "CORRECT" and "WRONG"
-    result = solve_and_check(num_1, num_2, operator, float(given_answer))            # solve task to check user
-    print()
-    print(result)                                                                    # print CORRECT" or "WRONG"
-    print()
+    result = solve_and_check(num_1, num_2, operator, float(given_answer))           # solve task to check user
+    print("YOUR ANSWER IS:", result)                                                                    # print CORRECT" or "WRONG"
+    print("=" * 100)
     if result == "CORRECT":
         statistic_dictionary["CORRECT"] = statistic_dictionary.get("CORRECT", 0) + 1 # count amount of "CORRECT". ADD VALUE to dictionary
     elif result == "WRONG":
@@ -69,14 +67,16 @@ while given_answer != "q":
     operator = random.choice(math_operators)
     print(num_1, operator, num_2)
     given_answer = check_input()
-
+    first_input = False
 # count OUTPUPUT
 
 if statistic_dictionary["WRONG"] > 0:
     output = statistic_dictionary["CORRECT"] / (statistic_dictionary["WRONG"] + statistic_dictionary["CORRECT"]) * 100
+elif first_input:
+    output = 0.0
 else:
     output = 100.0
 
 # print OUTPUT
-
+print("=" * 100)
 print(f"You have: {round(output, 1)}% correct answers")
